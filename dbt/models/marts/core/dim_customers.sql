c66ce56da0a19958e1695870aafe6e175192418d
Timur Suleyev <suleyevta@gmail.com>
2021-07-26 23:36:37 +0600

    Added lifetime_value column to dim_customers.sql model

--- dbt/models/marts/core/dim_customers.sql
+++ dbt/models/marts/core/dim_customers.sql
@@ -6,12 +6,18 @@ WITH customers AS (
     SELECT * FROM {{ ref('stg_customers') }}
 ), orders AS (
     SELECT * FROM {{ ref('stg_orders') }}
+), orders_payments AS (
+    SELECT order_id
+         , amount
+    FROM {{ ref('fct_orders') }}
 ), customer_orders AS (
-    SELECT customer_id
-         , min(order_date) AS first_order_date
-         , max(order_date) AS most_recent_order_date
-         , count(order_id) AS number_of_orders
-    FROM orders
+    SELECT o.customer_id
+         , MIN(o.order_date) AS first_order_date
+         , MAX(o.order_date) AS most_recent_order_date
+         , COUNT(o.order_id) AS number_of_orders
+         , SUM(COALESCE(op.amount, 0)) AS lifetime_value
+    FROM orders o
+    LEFT JOIN orders_payments op USING (order_id)
     GROUP BY 1
 ), final AS (
     SELECT customers.customer_id
@@ -20,6 +26,7 @@ WITH customers AS (
          , customer_orders.first_order_date
          , customer_orders.most_recent_order_date
          , coalesce(customer_orders.number_of_orders, 0) AS number_of_orders
+         , lifetime_value
     FROM customers
     LEFT JOIN customer_orders USING (customer_id)
 )
