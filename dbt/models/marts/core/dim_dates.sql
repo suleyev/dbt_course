ff9c7c97dfb82b696dec39487c7f3e96d6956d45
Timur Suleyev <suleyevta@gmail.com>
2021-07-29 00:28:15 +0600

    Added new model dim_dates.sql based on dbt_utils.date_spine macros

---
+++ dbt/models/marts/core/dim_dates.sql
@@ -0,0 +1,8 @@
+{{ config(schema='jaffle_shop') }}
+
+{{ dbt_utils.date_spine(
+    datepart="day",
+    start_date="cast('2020-01-01' as date)",
+    end_date="cast('2021-07-30' as date)"
+   )
+}}
