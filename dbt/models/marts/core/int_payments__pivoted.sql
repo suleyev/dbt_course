4cc7b4eb9e7aa8f0824bb6b668aec0b5c6448223
Timur Suleyev <suleyevta@gmail.com>
2021-07-29 00:36:28 +0600

    int_payments__pivoted.sql model refactored to use the new dim_dates model

--- dbt/models/marts/core/int_payments__pivoted.sql
+++ dbt/models/marts/core/int_payments__pivoted.sql
@@ -2,17 +2,21 @@
 {% set payment_methods = ['coupon', 'gift_card', 'bank_transfer', 'credit_card'] %}
 
 
-WITH payments AS (
+WITH dates AS (
+   SELECT CAST(date_day AS date) AS date
+   FROM {{ ref('dim_dates') }}
+), payments AS (
     SELECT created AS payment_date
          , paymentmethod AS payment_method
          , amount
     FROM stripe.payments
 )
 
-SELECT payment_date
-    {% for payment_method in payment_methods %}
+SELECT d.date
+    {% for payment_method in payment_methods -%}
      , SUM(CASE WHEN payment_method = '{{ payment_method }}' THEN amount ELSE 0 END) AS {{ payment_method }}_total
     {% endfor %}
-FROM payments
+FROM dates d
+LEFT JOIN payments p ON d.date = p.payment_date
 GROUP BY 1
 ORDER BY 1 DESC
