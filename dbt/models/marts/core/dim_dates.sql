268b9fd78f0c2dc8b43ad5a5ee01f5dc9fd85b76
Timur Suleyev <suleyevta@gmail.com>
2021-07-29 00:35:34 +0600

    Changed dates period in dim_dates.sql model

--- dbt/models/marts/core/dim_dates.sql
+++ dbt/models/marts/core/dim_dates.sql
@@ -2,7 +2,7 @@
 
 {{ dbt_utils.date_spine(
     datepart="day",
-    start_date="cast('2020-01-01' as date)",
-    end_date="cast('2021-07-30' as date)"
+    start_date="cast('2018-01-01' as date)",
+    end_date="cast('2019-01-01' as date)"
    )
 }}
